c3639e68233b438d23eba521c58a43d6b35d0ff2
ender019 <ender-fox@mail.ru>
2026-04-05 13:28:27 +0300

    init project

---
+++ src/main/java/com/vk/VkGrpcCRUD/VkGrpcCrudApplication.java
@@ -0,0 +1,13 @@
+package com.vk.VkGrpcCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkGrpcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkGrpcCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vk/VkGrpcCRUD/VkGrpcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.VkGrpcCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkGrpcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
